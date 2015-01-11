0e6c95a95bdbe74e95f175eb1f4e6ba6a097bd13
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2015-01-11 17:52:22 +0100

    Add EaseJS, Twitter Bootstrap and Jquery dependencies to composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel {
             new Pfe\Bundle\CoreBundle\PfeCoreBundle(),
             new Pfe\Bundle\ProviderBundle\PfeProviderBundle(),
             new Pfe\Bundle\GeonamesApiBundle\PfeGeonamesApiBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
